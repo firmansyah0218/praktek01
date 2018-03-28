7a54318e178809cdad7a53b04cccabbc3e41c811
firmansyah0218 <ferihabibi96@gmail.com>
2018-03-28 12:05:10 +0800

    membuat atribut dan behavior

--- src/praktek01/rumah.java
+++ src/praktek01/rumah.java
@@ -10,5 +10,17 @@ package praktek01;
  * @author Firman
  */
 public class rumah {
+    String pintu;
+    String jendela;
+    String fentilasi;
+    String tangga;
+    String atap;
     
+    void  cetakinfo(){
+            System.out.println("Pintu :"+pintu);
+            System.out.println("Jendela :"+jendela);
+            System.out.println("Fentilasi :"+fentilasi);
+            System.out.println("Tangga :"+tangga);
+            System.out.println("Atap :"+atap);
+    }
 }
